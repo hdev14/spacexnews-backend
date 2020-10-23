5c4af7c3ef37e966c1f23dbe7873beec853b7b3f
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-22 21:16:13 -0300

    refactor: refactor UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,65 +5,36 @@ class UserController {
   public async index (req: Request, res: Response) {
     const userCollection = Mongo.getCollection('users')
     const users = await userCollection.find({}).toArray()
+
     return res.status(200).json(users)
   }
 
   public async create (req: Request, res: Response) {
-    const data = req.body
     const usersCollection = Mongo.getCollection('users')
-    const newUser = (await usersCollection?.insertOne(data)).ops[0]
+    const newUser = (await usersCollection?.insertOne(req.body)).ops[0]
+
     return res.status(201).json(newUser)
   }
 
   public async update (req: Request, res: Response) {
-    // try {
-    //   const repository = getRepository(User)
-    //   const user = await repository.findOneOrFail(req.params.id)
-    //   repository.merge(user, req.body)
-    //   const updatedUser = await repository.save(user)
-    //   return res.json(updatedUser)
-    // } catch (err) {
-    //   return notfoundError(err)
-    // }
     const data = req.body
     const { id } = req.params
-    const userCollection = Mongo.getCollection('users')
 
     try {
+      const userCollection = Mongo.getCollection('users')
       const { result } = await userCollection.updateOne({ _id: id }, { $set: data })
+
       return result.ok && res.status(200).json()
     } catch (err) {
       return res.status(400).json()
     }
   }
 
-  public async updatePhoto (req: Request, res: Response) {
-    // try {
-    //   const { filename } = req.file
-    //   const repository = getRepository(User)
-    //   const user = await repository.findOneOrFail(req.userId)
-
-    //   if (user.photo) {
-    //     const tmpPath = path.resolve(__dirname, '..', '..', 'tmp')
-    //     const filePath = path.join(tmpPath, user.photo)
-
-    //     const file = await fs.promises.stat(filePath)
-    //     if (file.isFile()) {
-    //       await fs.promises.unlink(filePath)
-    //     }
-    //   }
-    //   user.photo = filename
-    //   const updatedUser = await repository.save(user)
-    //   return res.json(updatedUser)
-    // } catch (err) {
-    //   return notfoundError(err)
-    // }
-  }
-
   public async delete (req: Request, res: Response) {
     const { id } = req.params
     const userCollection = Mongo.getCollection('users')
     const { result } = await userCollection.deleteOne({ _id: id })
+
     return result.ok && res.status(200).json()
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,4 @@
 import { Router } from 'express'
-import multer from 'multer'
-
-import multerStorageConfig from './configs/multer-storage'
 
 // Validators
 import UserValidator from './validators/UserValidator'
@@ -17,8 +14,5 @@ router.post('/users', UserValidator.create, userController.create)
 router.get('/users', userController.index)
 router.put('/users/:id', UserValidator.update, userController.update)
 router.delete('/users/:id', userController.delete)
-
-const upload = multer({ storage: multerStorageConfig })
-router.patch('/users/photo', upload.single('photo'), userController.updatePhoto)
 
 export default router
