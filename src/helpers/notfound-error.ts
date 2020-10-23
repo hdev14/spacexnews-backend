b39c3f735a6178f8980867cbedbf72df5696c682
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-23 09:45:02 -0300

    refactor: remove helper

--- src/helpers/notfound-error.ts
+++
@@ -1,9 +0,0 @@
-import RequestError from '../errors/RequestError'
-
-export default function notfoundError (error: Error): void {
-  if (error.name === 'EntityNotFound') {
-    throw new RequestError(error.message, error.name, 404)
-  }
-
-  throw error
-}
